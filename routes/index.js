8f43220ead0ccad7cf3fa33de47d6e571f1f4e1a
Santosh Parashetti <santu.parashetti@gmail.com>
2016-01-10 13:01:21 +0530

    form creation started

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{title:'Shipment Management', message: '@Lastmile Logistics'});
+  res.render('createform',{title:'Shipment Management', message: ''});
 });
 
 router.get('/createform', function(req, res, next) {
-  res.render('createform',{title:'Create Userform', message: '@Lastmile Logistics'});
+  res.render('createform',{title:'Create Userform', message: ''});
 });
 
 module.exports = router;
